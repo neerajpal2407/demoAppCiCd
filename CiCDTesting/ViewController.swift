b0490db8fab874125d4b047a5b1a466d1b8b24b4
neerajpal2407 <119958959+neerajpal2407@users.noreply.github.com>
2025-05-01 19:36:06 +0300

    first commit

---
+++ CiCDTesting/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CiCDTesting
+//
+//  Created by Neeraj pal on 01/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CiCDTestingTests/CiCDTestingTests.swift
@@ -0,0 +1,17 @@
+//
+//  CiCDTestingTests.swift
+//  CiCDTestingTests
+//
+//  Created by Neeraj pal on 01/05/25.
+//
+
+import Testing
+@testable import CiCDTesting
+
+struct CiCDTestingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
